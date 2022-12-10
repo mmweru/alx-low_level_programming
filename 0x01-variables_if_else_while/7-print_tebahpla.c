962892636b2bd881096c00a0789ce0328fdef73f
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-09 20:35:27 -0800

    Shell programming

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point of the function
+* outputs alphabets in reverse
+* Return : Always 0.
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}

b802d2d9e791facdde4015c50117b56051c11293
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-09 17:58:17 -0800

    Shell programming

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* main - Entry point of the code
+* code checks whether an input is negative, positive or zero
+* Return:  Always 0.
+*/
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}

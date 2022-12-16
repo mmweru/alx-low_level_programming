a3d29ca45d76ac4025dc8525cf7d165c13811cc8
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-15 16:48:22 -0800

    C programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - Entry point of the function
+* outputs the alphabets
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

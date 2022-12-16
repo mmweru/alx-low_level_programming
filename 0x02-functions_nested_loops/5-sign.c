1918184a4bcba8f31342f3d3d7211c3074eedbc4
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-15 18:18:56 -0800

    C programming

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - checks whether a sign is 0, <, > 
+* outputs 1 and 0 respectively
+* @n: integer to be tested
+* Return: 1, 0 and -1 respectively
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

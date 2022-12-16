f309551f167905592aaa25b7d3097c3f400f159c
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-15 21:00:04 -0800

    C programming

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+* times_table - outputs multiple of 9
+* Return: does not have a return value.
+*/
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			res = i * j;
+			if ((res / 10) == 0)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+

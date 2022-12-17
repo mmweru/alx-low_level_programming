1fd0e757b7e021559af7b54dfc4574613c9ce65f
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-17 05:36:14 -0800

    C programming

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <main.h>
+/**
+* main - Entry point of the function
+* outputs the summation of the multiples of 3 and 5
+* Return: always 0.
+*/
+int main(void)
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+		printf("%d ", sum);
+
+		return (0);
+	}
+}

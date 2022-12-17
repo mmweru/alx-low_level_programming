ca43998b925dc6233bd36e1bb1c30b7b51666caa
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-17 05:43:09 -0800

    C programming

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,14 @@
 int main(void)
 {
 	int n;
-	int sum = 0;
+	int sum;
 
 	for (n = 0; n < 1024; n++)
 	{
-		if (n % 3 == 0 || n % 5 == 0)
-		{
+		if ((n % 3 == 0) || (n % 5 == 0))
 			sum += n;
-		}
+	}
 		printf("%d ", sum);
 
 		return (0);
-	}
 }

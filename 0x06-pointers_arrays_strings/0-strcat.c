3cc276c33a827a4af8ffa48468ce5707ca37880c
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-21 12:34:57 -0800

    C programming

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@ char *_strcat(char *dest, char *src)
 	int index = 0;
 	int j = 0;
 
-	while (dest[i++])
+	while (dest[index++])
 		j++;
 	for (index = 0; src[index]; index++)
 		dest[j++] = src[index];

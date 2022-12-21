fb509aa204dd5f3171788e328ca3b4b4d9ff2153
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-20 19:58:32 -0800

    C programming

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point of the function
+* outputs a randomly generated password
+* Return: Always 0.
+*/
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", pass);
+	return (0);
+}

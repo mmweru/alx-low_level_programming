b84f5791544827ac76f37d70a78afb0c34b8e9a7
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-21 17:23:21 -0800

    C programming

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* reverse_array - function to print the reverse order of an array
+* @a: the pointer to the array
+* @n: the size of the array
+* Return: Nothing.
+*/
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		a[i];
+	}
+}

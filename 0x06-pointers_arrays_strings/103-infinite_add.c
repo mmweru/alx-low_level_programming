7f933c0c49bf210d407acead521042bde03eb824
Maryann Wambui <mwerumaryann@gmail.com>
2022-12-23 02:13:11 -0800

    C programming

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* infinite_add - function that adds two numbers
+* @n1: the first number to be added
+* @n2: the second number to be added
+* @r: the pointer to store the result
+* @size_r: the size of the buffer
+* Return: the result of the summation.
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k1, k2;
+	char a1[n];
+	char a2[n];
+
+	for (i =0; i < size_r; i++)
+	{
+		a1[i] = n1[i];
+		a2[i] = n2[i];
+	}
+	k1 = 0;
+	k2 = 0;
+	for (j = 0; j < size_r; j++, i++)
+	{
+		k1 = 10 * k1 + a1[i];
+		k2 = 10 * k2 + a2[i];
+	}
+	r = k1 + k2;
+	return (r);
+}

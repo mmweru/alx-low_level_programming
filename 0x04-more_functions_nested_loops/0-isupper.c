8c9bea2286c2c5b56948ec32f64d3cebabe37645
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-01 22:33:12 -0800

    Some correcions made on the file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
-* _isupper- Entry point of the function.
+* _isupper - Entry point of the function
 * checks whether the character is in uppercase
 * @c: character to be checked
 * Return: returns 1 if uppercase and 0 if otherwise.

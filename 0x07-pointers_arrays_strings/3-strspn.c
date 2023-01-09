7fd020afe668e8ae572c5dc5551bb90eebff2c35
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-09 06:51:53 -0800

    More adjustments made on the file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,9 +7,10 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j;
-	int k = 0;
+	int i, k, j;
 
+	i = 0;
+	k = 0;
 	while (s[i] != '\0')
 	{
 		for (j = 0; accept[j] != '\0'; j++)
@@ -20,7 +21,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 			if (accept[j + 1] == '\0' && s[i] != accept[j])
-				return k;
+				return (k);
 		}
 		i++;
 	}

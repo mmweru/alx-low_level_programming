b57264c2004fe374ca9e36b4f7dd7d2dc0758026
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-09 05:03:20 -0800

    function that copies bytes from one memory area to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memcpy - a function that copies n bytes from memory area src to memory area dest
+* @n: the size of the memory in bytes
+* @src: the first memory area
+* @dest: the second memory area
+* Return: a pointer to dest.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int c = n;
+
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < c; i++)
+			dest[i] = src[i];
+	}
+	return (dest);
+}

6c830f1721eb23922a3b72b6dce9b6d082f88d51
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-11 17:39:09 -0800

    Program to redirect a pointer to a string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* set_string - sets a ponter to a character
+* @s: pointer to be set
+* @to: character to be set
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

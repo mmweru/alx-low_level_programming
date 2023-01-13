b8790e8bf60ccf565249cea8722e83d4499cd40d
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-13 02:16:24 -0800

    Adjustments made on the file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size != 0)
+	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -16,6 +16,7 @@ void print_square(int size)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
 	}
 	else

d9ecebd4ed5e8776e7249373c70967d26d453b4a
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-13 05:43:10 -0800

    More changes made on the file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,17 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (a % 3 == 0)
-		{
-			printf("Fizz ");
-		}
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
 		else
 		{
-			printf("%i ", a);
+			printf("%d ", a);
 		}
 	}
 	printf("\n");

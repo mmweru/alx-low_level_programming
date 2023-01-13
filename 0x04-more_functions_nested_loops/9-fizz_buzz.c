41c5bcd1afb177fb28aa7760a7e951c833ab4675
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-13 02:51:04 -0800

    A program on FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point of the function
+* Return: Always 0
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	return (0);
+}

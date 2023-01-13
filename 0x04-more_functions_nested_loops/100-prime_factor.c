a21bf13ddfe98c56b606944398f6036ee6fae87d
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-13 12:07:55 -0800

    Changes made on the file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,12 +5,16 @@
 */
 int main(void)
 {
-	unsigned long int i = 3, a = 612852475143;
+	unsigned long int i, a;
 
+	i = 3;
+	a = 612852475143;
 	for(; i < 12057; i += 2)
 	{
 		while (a % i == 0 && a != i)
+		{
 			a /= i;
+		}
 	}
 	printf("%lu\n", a);
 	return (0);

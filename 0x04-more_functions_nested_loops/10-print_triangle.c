eedfd6ce2c9c74640d67582f5ff90e2eb7584559
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-13 06:42:12 -0800

    Program to output a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_triangle - prints a triangle
+* @size: the size of triangle
+* Return: Nothing.
+*/
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b >= 1; b--)
+			{
+				_putchar(' ');
+			}
+			for (c = 1; c <= a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 int main(void);
+void print_triangle(int size);

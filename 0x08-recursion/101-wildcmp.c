96a38c5572fd61a0c0cf716e358e659507e6eede
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-17 03:36:22 -0800

    Checks if 2 strings are the same

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* wildcmp - compares strings
+* @s1: the first string
+* @s2: the second string
+* Return: 1 success 0 failure.
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if(*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 =='\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}

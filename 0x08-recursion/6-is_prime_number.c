62a3e8c7db633213efbf78187c86a73144bd394e
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-17 01:53:13 -0800

    Adjustments made on the prime checker

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,23 +6,20 @@
 */
 int prime(int a)
 {
-	int x[4] = {2, 3, 5, 7};
-	int i;
-
 	while (a > 1)
 	{
-		for (i = 0; i < 4; i++)
+		if ((a != 2) || (a != 3) || (a != 5) || (a != 7))
 		{
-			if (a % x[i] != 0)
-			{
-				return (1);
-			}
-			else if (a % x[i] == 0)
+			if ((a % 2 == 0) || (a % 3 == 0) || (a % 5 == 0) || (a % 7 == 0))
 			{
 				return (0);
 			}
-			return (0);
+			else
+			{
+				return (1);
+			}
 		}
+		return (1);
 	}
 	return (0);
 }

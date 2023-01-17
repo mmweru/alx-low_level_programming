5cb83ad154415effe54b51ee98f146ea35247583
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-17 03:39:20 -0800

    Changes made on the file

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,11 +9,11 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if(*s2 != '\0' && *s2 == '*')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
-		return (*s2 =='\0');
+		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
 	{

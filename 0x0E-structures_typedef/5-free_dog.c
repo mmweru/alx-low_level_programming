1c920479bf2a1841caea9979180ddba80aaf0b9d
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-20 02:51:46 -0800

    Program to free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - function that frees a dog
+* @d: pointer for struct
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+			free(d->name);
+		}
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}

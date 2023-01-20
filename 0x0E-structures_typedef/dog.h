4c0ff7542df2a9a59d303b51057acab490fa601f
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-20 00:48:26 -0800

    Documentation added to the code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* dog - the struct of dog
+* @name: the name of dog
+* @age: the age of dog
+* @owner: the owner of dog.
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif

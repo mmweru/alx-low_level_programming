48d2d5dc3951cd29d9d5b4be8dd97c459385d0d8
Maryann Wambui <mwerumaryann@gamil.com>
2023-01-20 04:37:05 -0800

    Adjustments made on the file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
-* print_name - outputs the name
+* print_name - outputs the
+* name
 * @name: name to be outputed
 * @f: pointer to name
 * Return: nothing.
@@ -9,7 +10,9 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 
 	f(name);
 }

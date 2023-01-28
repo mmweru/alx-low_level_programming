5b939107da4c9fa5264759f4d06f123ec70b848c
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-28 06:55:59 -0800

    Changes made in the repo

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,8 +8,8 @@
 */
 char *argstostr(int ac, char **av)
 {
- 	char *str;
-	int count = 0, a =0, b = 0, c = 0;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);

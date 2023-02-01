77f4abb99756934824e41c716b25f5368ddc0020
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-31 16:51:30 -0800

    Craetion of a modified print_list file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - outputs a
+* list of all elements
+* @h: list_h list
+* Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}

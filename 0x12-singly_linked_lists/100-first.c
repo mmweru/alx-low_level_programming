710fb5dc389fe0d69aaf6c3eadec8715fcc4d839
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-31 18:29:47 -0800

    prints a string before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* bmain - function executed before
+* the main
+* Return: Nothing.
+*/
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}

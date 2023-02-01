f1f2d5cf0c54202f7396a20df8ff5cd3d07bbed2
Maryann Wambui <mwerumaryann@gmail.com>
2023-01-31 16:35:06 -0800

    Linked lists in c to output a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+* print_list - outputs all elements
+* of a list
+* @h: list_t list
+* Return: number of nodes in h.
+*/
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}

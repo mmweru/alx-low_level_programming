79c507a7c883b5c480fc71aaf2309386eadd847e
Maryann Wambui <mwerumaryann@gmail.com>
2023-02-01 18:15:01 -0800

    A new file has been created

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
-* free_listint2 - function that
-* frees a list
-* head: pointer to the head
-* Return: Nothing.
+* free_listint2 - frees a
+* linked list
+* @head: pointer to first node
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *new;
+	listint_t *temp;
 
 	if (head == NULL)
+	{
 		return;
-
+	}
 	while (*head)
 	{
-		new = (*head)->next;
+		temp = (*head)->next;
 		free(*head);
-		*head = new;
+		*head = temp;
 	}
 	*head = NULL;
 }

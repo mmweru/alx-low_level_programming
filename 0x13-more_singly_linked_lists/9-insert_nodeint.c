762a2f7f40a878a79f4ec2e2a5fc39b7a182469b
Maryann Wambui <mwerumaryann@gmail.com>
2023-02-02 16:45:54 -0800

    Changes made in the file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,15 +17,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
+
 	node->n = n;
 	node->next = NULL;
+
 	if (!*head && !idx)
 	{
 		*head = node;
 		return (node);
 	}
+
 	else if (!*head && idx)
+	{
 		return (NULL);
+	}
 	else if (!idx)
 	{
 		node->next = temp;
@@ -37,8 +42,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
+
 	node->next = temp->next;
 	temp->next = node;
 
 	return (node);
-} 
+}

db35e446e8e06af96651ae93c3e913da61372e55
Maryann Wambui <mwerumaryann@gmail.com>
2023-02-02 16:25:31 -0800

    prints all elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
-* print_listint - prints all elements
-* of a list
-* @h: pointer to the elements
+* print_listint - prints elements of
+* a linked list
+* @h: pointer to 1st node
 * Return: number of nodes.
 */
 size_t print_listint(const listint_t *h)

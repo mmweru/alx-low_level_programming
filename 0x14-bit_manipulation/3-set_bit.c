813b7e54b397846734cfdcd9b0111eb2be571cc4
Maryann Wambui <mwerumaryann@gmail.com>
2023-02-07 01:47:09 -0800

    Replaces the index with 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* set_bit - converts the value
+* of an index at a particular position
+* @*n: integer to be set
+* @index: index of the integer
+* Return: 1 if worked -1 error
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 53 || !n)
+		return (-1);
+	(*n |= 1 << index);
+	return (1);
+}
